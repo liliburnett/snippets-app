18ca5ccf79fcc94082d208c50385c5fc329e504b
Lianna Burnett <lilib@pengown.com>
2017-03-29 23:39:49 -0500

    Added functionality to functions

--- snippets.py
+++ snippets.py
@@ -1,42 +1,58 @@
 import logging
 import argparse
+import psycopg2
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
 
-def put(name, snippet):
-    """
-    Store a snippet with an associated name.
+logging.debug("Connecting to PostgreSQL")
+connection = psycopg2.connect(database="snippets", user="lili")
+logging.debug("Database connection established.")
 
-    Returns the name and the snippet
-    """
+def put(name, snippet):
+    """Store a snippet with an associated name."""
     logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+
+    cursor = connection.cursor()
+    command = "insert into snippets values (%s, %s)"
+    cursor.execute(command, (name, snippet))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
     return name, snippet
 
 def get(name):
-    """ Retrieve the snippet with a given name.
-
-    If there is no such snippet, return '404: Snippet Not Found'.
-
-    Returns the snippet.
-    """
+    """ Retrieve the snippet with a given name."""
     logging.error("FIXME: Unimplemented - get({!r})".format(name))
-    return ""
 
-def update(name, snippet):
-    """ Update the snippet with a given name.
+    cursor = connection.cursor()
+    command = "select * from snippets where keyword=%s"
+    cursor.execute(command, (name,))
+    snippet = cursor.fetchone()
+    connection.commit()
+    logging.debug("Snippet retrieved.")
+    return snippet
 
-    Returns the updated snippet.
-    """
+def update(snippet, name):
+    """ Update the snippet with a given name."""
+
     logging.error("FIXEME: Unimplemented - update({!r})".format(name))
-    return name, snippet
+    cursor = connection.cursor()
+    command = "update snippets set message=%s where keyword=%s"
+    cursor.execute(command, (snippet, name))
+    connection.commit()
+    logging.debug("Snippet updated successfully.")
+    return snippet, name
 
 def delete(name):
-    """ Delete the snippet with a given name.
+    """ Delete the snippet with a given name."""
 
-    Returns deleted snippet name.
-    """
     logging.error("FIXME: Unimplemented - delete({!r})".format(name))
+
+    cursor = connection.cursor()
+    command = "delete from snippets where keyword=%s"
+    cursor.execute(command, (name,))
+    connection.commit()
+    logging.debug("Snippet successfully deleted.")
     return name
 
 def main():
@@ -57,11 +73,11 @@ def main():
     get_parser = subparsers.add_parser("get", help="Get a snippet")
     get_parser.add_argument("name", help="Name of the snippet")
 
-     Subparser for the update command
+    # Subparser for the update command
     logging.debug("Constructing update subparser")
     update_parser = subparsers.add_parser("update", help="Update a snippet")
-    update_parser.add_argument("name", help="name of the snippet")
     update_parser.add_argument("snippet", help="updated snippet text")
+    update_parser.add_argument("name", help="name of snippet")
 
     # Subparser for the delete command
     logging.debug("Constructing delete subparser")
@@ -71,7 +87,7 @@ def main():
     arguments = parser.parse_args()
     arguments = vars(arguments)
     command = arguments.pop("command")
-
+    print(arguments["name"])
     if command == "put":
         name, snippet = put(name=arguments["name"], snippet=arguments["snippet"])
         print("Stored {!r} as {!r}".format(snippet, name))
@@ -79,11 +95,11 @@ def main():
         snippet = get(name=arguments["name"])
         print("Retrieved snippet: {!r}".format(snippet))
     elif command == "update":
-        snippet = update(**arguments)
-        print("Updated {!r} with {!r}".format(name, snippet))
+        snippet, name = update(snippet=arguments["snippet"], name=arguments["name"])
+        print("Updated {!r} with {!r}".format(snippet, name))
     elif command == "delete":
-        snippet = delete(**arguments)
-        print("Deleted {!r}".format(snippet))
+        name = delete(name=arguments["name"])
+        print("Deleted {!r}".format(name))
 
 if __name__ == "__main__":
     main()
